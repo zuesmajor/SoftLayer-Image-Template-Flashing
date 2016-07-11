113b2ee76d5c422921c97cd37187c87e17c6c5c2
Patrick Ryan <ryanpa@Patricks-MacBook-Pro-3.local>
2016-07-11 15:07:01 -0700

    changed the .gitignore for pycharm. Changed the main called for the role

--- Db2C Ansible/library/ImageModule.py
+++ Db2C Ansible/library/ImageModule.py
@@ -13,19 +13,28 @@ options:
 '''
 
 try:
-	import SoftLayer
+    import SoftLayer
 
-client = SoftLayer.Client(username='USER_NAME', 
-	api_key='API_KEY')
+client = SoftLayer.Client(username='USER_NAME',
+                          api_key='API_KEY')
+imageName = ''
+
 
 def searchPrivateImage(module):
-	if module.params.get('imagename'):
-		imagename = module.params.get('imagename')
 
-	mask = "mask[id,name,note]"
-	imageTemplates = client['SoftLayer_Account'].getPrivateBlockDeviceTemplateGroups(mask=mask)
+    # grab the name from the module to choose which private image to flash to the new procured VM
 
-	for template in imageTemplates:
-		try:
-			if template['name'] is imagename:
-				# might need to grab id to flash???
+    if module.params.get('imagename'):
+        imagename = module.params.get('imagename')
+
+    mask = "mask[id,name,note]"
+    imageTemplates = client['SoftLayer_Account'].getPrivateBlockDeviceTemplateGroups(mask=mask)
+
+    for template in imageTemplates:
+        try:
+            if template['name'] is imagename:
+        # might need to grab id to flash???
+
+
+if __name__ == '__main__':
+    main()
