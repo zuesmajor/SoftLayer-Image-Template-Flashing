0dc8484e1f7ccce2044d7665c7ef1e569a216ce3
Patrick Ryan <ryanpa@patricks-mbp-3.usca.ibm.com>
2016-07-13 16:08:13 -0700

    added REST call to create or load an image onto an existing

--- Db2C Ansible/library/ImageModule.py
+++ Db2C Ansible/library/ImageModule.py
@@ -2,39 +2,31 @@ DOCUMENTATION = '''
 
 ---
 
-module: PrivateImageSearch
-options:
-	instance_id:
-		required: false
-		default: null
 
-	imagename:
-		required: true
+
 '''
 
-try:
-    import SoftLayer
 
-client = SoftLayer.Client(username='USER_NAME',
-                          api_key='API_KEY')
-imageName = ''
+try:
+  import SoftLayer
+  from pprint import pprint as pp
 
 
 def searchPrivateImage(module):
 
     # grab the name from the module to choose which private image to flash to the new procured VM
 
-    if module.params.get('imagename'):
-        imagename = module.params.get('imagename')
+  if module.params.get('namePrivImage'):
+    namePrivImage = module.params.get('namePrivImage')
 
-    mask = "mask[id,name,note]"
-    imageTemplates = client['SoftLayer_Account'].getPrivateBlockDeviceTemplateGroups(mask=mask)
+  mask = "mask[id,name,note]"
+  imageTemplates = client['SoftLayer_Account'].getPrivateBlockDeviceTemplateGroups(mask=mask)
 
-    for template in imageTemplates:
-        try:
-            if template['name'] is imagename:
+  for template in imageTemplates:
+    try:
+      if template['name'] == namePrivImage:
+        copiedImage = template
+        pp(copiedImage)
+        # going to be used to copy the image from the private list
         # might need to grab id to flash???
 
-
-if __name__ == '__main__':
-    main()
